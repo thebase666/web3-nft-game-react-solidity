9719865facda29d85bcdf3470e60f2da364356ff
thebase <73353819+thebase666@users.noreply.github.com>
2022-05-07 19:31:33 +0700

    Update MyEpicGame.sol

--- solidity/contracts/MyEpicGame.sol
+++ solidity/contracts/MyEpicGame.sol
@@ -29,20 +29,14 @@ contract MyEpicGame is ERC721 {
         uint256 maxHp;
         uint256 attackDamage;
     }
-
+    // struct CharacterAttributes       struct BigBoss {
     BigBoss public bigBoss; //初始数据用bigBoss 和 defaultCharacters数组保存
+    CharacterAttributes[] defaultCharacters;
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes; // 每个nft的状态
+    mapping(address => uint256) public nftHolders; //每个用户的nft 也是可更改可增加新用户的
 
-    // The tokenId is the NFTs unique identifier, it's just a number that goes
-    // 0, 1, 2, 3, etc.
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-
-    CharacterAttributes[] defaultCharacters; //保存nft名字图片等信息 不可更改
-
-    // permanent global variables on the contract
-    // nftHolderAttributes保存每个nft的状态 可更改 用mapping
-    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
-    mapping(address => uint256) public nftHolders; //每个用户的nft 也是可更改可增加新用户的
 
     event CharacterNFTMinted(
         address sender,
@@ -64,12 +58,7 @@ contract MyEpicGame is ERC721 {
         string memory bossImageURI,
         uint256 bossHp,
         uint256 bossAttackDamage
-    )
-        // Below, you can also see I added some special identifier symbols for our NFT.
-        // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
-        // Heroes and HERO. Remember, an NFT is just a token!
-        ERC721("Heroes", "HERO")
-    {
+    ) ERC721("Heroes", "HERO") {
         bigBoss = BigBoss({
             name: bossName,
             imageURI: bossImageURI,
@@ -77,13 +66,6 @@ contract MyEpicGame is ERC721 {
             maxHp: bossHp,
             attackDamage: bossAttackDamage
         });
-
-        console.log(
-            "Done initializing boss %s w/ HP %s, img %s",
-            bigBoss.name,
-            bigBoss.hp,
-            bigBoss.imageURI
-        );
 
         for (uint256 i = 0; i < characterNames.length; i += 1) {
             defaultCharacters.push(
@@ -108,24 +90,13 @@ contract MyEpicGame is ERC721 {
             );
         }
 
-        // I increment _tokenIds here so that my first NFT has an ID of 1.
-        // More on this in the lesson!
         _tokenIds.increment();
     }
 
-    // Users would be able to hit this function and get their NFT based on the
-    // characterId they send in!
     function mintCharacterNFT(uint256 _characterIndex) external {
-        // Get current tokenId (starts at 1 since we incremented in the constructor).
         uint256 newItemId = _tokenIds.current();
-
-        // The magical function! Assigns the tokenId to the caller's wallet address.
-        console.log("_safeMint start");
         _safeMint(msg.sender, newItemId);
-        console.log("_safeMint finish");
 
-        // We map the tokenId => their character attributes. More on this in
-        // the lesson below.
         // mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
         nftHolderAttributes[newItemId] = CharacterAttributes({
             characterIndex: _characterIndex,
@@ -136,22 +107,12 @@ contract MyEpicGame is ERC721 {
             attackDamage: defaultCharacters[_characterIndex].attackDamage
         });
 
-        console.log(
-            "Minted NFT w/ tokenId %s and characterIndex %s",
-            newItemId,
-            _characterIndex
-        );
-
-        // Keep an easy way to see who owns what NFT.
         nftHolders[msg.sender] = newItemId;
-
-        // Increment the tokenId for the next person that uses it.
         _tokenIds.increment();
         emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
     }
 
-    // tokenURI写在合约里面 就是override重写合约中自带的tokenURI 给nft传metadata
-    // 执行顺序 到什么位置执行的tokenURI _tokenId是多少
+    // override重写合约中自带的tokenURI 给nft传metadata
     function tokenURI(uint256 _tokenId)
         public
         view
@@ -194,51 +155,31 @@ contract MyEpicGame is ERC721 {
     }
 
     function attackBoss() public {
-        // Get the state of the player's NFT.
-        // In contrast, if we were to use memory instead of storage it would create a local copy of the variable within the scope of the function. That means if we did player.hp = 0 it would only be that way within the function and wouldn't change the global value.
+        // storage的player直接挂钩map表 改动player就改动map表 tokenURI和checkIfUserHasNFT返回的就是map表内容
+        // tokenURI给nftmarket返回状态 checkIfUserHasNFT给前端react返回状态
+        // 只有这个位置用来storage 其他都是memory
+        // function getBigBoss() public view returns (BigBoss memory) {return bigBoss; }
+        // 直接调用初始的bigBoss变量修改 bigBoss不是nft 初始的defaultCharacters没有改
         uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
         CharacterAttributes storage player = nftHolderAttributes[
             nftTokenIdOfPlayer
         ];
-        //player直接挂钩attributes了 改动player就改动attributes了
 
-        console.log(
-            "\nPlayer w/ character %s about to attack. Has %s HP and %s AD",
-            player.name,
-            player.hp,
-            player.attackDamage
-        );
-        console.log(
-            "Boss %s has %s HP and %s AD",
-            bigBoss.name,
-            bigBoss.hp,
-            bigBoss.attackDamage
-        );
-
-        // Make sure the player has more than 0 HP.
         require(player.hp > 0, "Error: character must have HP to attack boss.");
-
-        // Make sure the boss has more than 0 HP.
         require(bigBoss.hp > 0, "Error: boss must have HP to attack boss.");
 
-        // Allow player to attack boss.
         if (bigBoss.hp < player.attackDamage) {
             bigBoss.hp = 0;
         } else {
             bigBoss.hp = bigBoss.hp - player.attackDamage;
         }
-
-        // Allow boss to attack player.
         if (player.hp < bigBoss.attackDamage) {
             player.hp = 0;
         } else {
             player.hp = player.hp - bigBoss.attackDamage;
         }
-
-        // Console for ease.
         console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
         console.log("Boss attacked player. New player hp: %s\n", player.hp);
-
         emit AttackComplete(msg.sender, bigBoss.hp, player.hp);
     }
 
@@ -251,6 +192,7 @@ contract MyEpicGame is ERC721 {
         uint256 userNftTokenId = nftHolders[msg.sender];
         // If the user has a tokenId in the map, return their character.
         if (userNftTokenId > 0) {
+            //查询状态时 也返回map表
             return nftHolderAttributes[userNftTokenId];
         }
         // Why do we do userNftTokenId > 0? Well, basically there's no way to check if a key in a map exists. We set up our map like this: mapping(address => uint256) public nftHolders. No matter what key we look for, there will be a default value of 0.
@@ -270,6 +212,6 @@ contract MyEpicGame is ERC721 {
     }
 
     function getBigBoss() public view returns (BigBoss memory) {
-        return bigBoss; //直接是合约的public变量 直接调用直接改 合约的函数内部声明变量才需要memory storage
+        return bigBoss; //BigBoss memory
     }
 }
